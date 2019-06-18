89b08fa3c216de80c1ddf0211344403c0ddaccac
sunaga <40333110+konnpeitoon@users.noreply.github.com>
2019-06-18 17:00:46 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/threejs-react-example/precache-manifest.dfdc9be54a83f8954e2a5e3278e52dd9.js"
+  "/precache-manifest.d088b200a21e699a05b60ff37372a840.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/threejs-react-example/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
